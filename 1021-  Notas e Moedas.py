bd4722c9ac3cd4ae366dbd0abca2290e2e734df2
Calixto Neto <joaocalixto2@hotmail.com>
2018-02-25 02:16:46 -0300

    accepted

--- 1021-  Notas e Moedas.py
+++ 1021-  Notas e Moedas.py
@@ -1,59 +1,70 @@
-notas = float(input())
-valor = notas
-nota100 = 0
-nota50 = 0
-nota20 = 0
-nota10 = 0
-nota5 = 0
-nota2 = 0
-nota1 = 0
-m050 = 0
-m25 = 0
-m10 = 0
-m5 = 0
-m01 = 0
+n100 = 0
+n50 = 0
+n20 = 0
+n10 = 0
+n5 = 0
+n2 = 0
+n1 = 0
+n050 = 0
+n025 = 0
+n010 = 0
+n005 = 0
+n001 = 0
 
+n = float(input())
 
-#contando notas
-while (notas >= 100.00):
-    notas = notas -100.00
-    nota100 += 1
-while (notas >= 50.00):
-    notas = notas - 50.00
-    nota50 += 1
-while (notas >= 20.00):
-    notas = notas - 20.00
-    nota20 += 1
-while (notas >= 10.00):
-    notas = notas - 10.00
-    nota10 += 1
-while (notas >= 5.00):
-    notas = notas - 5.00
-    nota5 += 1
-while (notas >= 2.00):
-    notas = notas - 2.00
-    nota2 += 1
-#contando moedas
-while (notas >= 1):
-    notas = notas - 1
-    nota1 += 1
-while notas > 0.50 :
-    notas = notas - 0.50
-    m050 += 1
-while notas >= 0.25 :
-    notas = notas - 0.25
-    m25 += 1
-while notas >= 0.10 :
-    notas = notas - 0.10
-    m10 += 1
-while notas >= 0.05 :
-    notas = notas - 0.05
-    m5 += 1
-while notas >= 0.01 :
-    notas = notas - 0.01
-    m01 += 1
+if (n >= 0 and n < 1000000):
+    while (True):
+        if (n >= 100):
+            n100 = n100 + 1
+            n = n - 100
+        elif (n >= 50):
+            n50 = n50 + 1
+            n = n - 50
+        elif (n >= 20):
+            n20 = n20 + 1
+            n = n - 20
+        elif (n >= 10):
+            n10 = n10 + 1
+            n = n - 10
+        elif (n >= 5):
+            n5 = n5 + 1
+            n = n - 5
+        elif (n >= 2):
+            n2 = n2 + 1
+            n = n - 2
+        elif (n >= 1):
+            n1 = n1 + 1
+            n = n - 1
+        elif (n >= 0.5):
+            n050 = n050 + 1
+            n = n - 0.5
+        elif (n >= 0.25):
+            n025 = n025 + 1
+            n = n - 0.25
+        elif (n >= 0.10):
+            n010 = n010 + 1
+            n = n - 0.10
+        elif (n >= 0.05):
+            n005 = n005 + 1
+            n = n - 0.05
+        elif (n >= 0.01 or n >= 0.00999999999999):
+            n001 += 1
+            n -= 0.01 or 0.00999999999999
+        else:
+            break
 
-
-print("NOTAS:\n{} nota(s) de R$ 100,00\n{} nota(s) de R$ 50,00\n{} nota(s) de R$ 20,00\n{} nota(s) de R$ 10,00\n{} nota(s) de R$ 5,00\n{} nota(s) de R$ 2,00".format(nota100,nota50,nota20,nota10,nota5,nota2))
-print("MOEDAS:\n{} moeda(s) de R$ 1.00\n{} moeda(s) de R$ 0.50\n{} moeda(s) de R$ 0.25\n{} moeda(s) de R$ 0.10\n{} moeda(s) de R$ 0.05\n{} moeda(s) de R$ 0.01".format(nota1,m050,m25,m10,m5,m01))
-
+    print('NOTAS:')
+    print(n100, 'nota(s) de R$ 100.00')
+    print(n50, 'nota(s) de R$ 50.00')
+    print(n20, 'nota(s) de R$ 20.00')
+    print(n10, 'nota(s) de R$ 10.00')
+    print(n5, 'nota(s) de R$ 5.00')
+    print(n2, 'nota(s) de R$ 2.00')
+    print('MOEDAS:')
+    print(n1, 'moeda(s) de R$ 1.00')
+    print(n050, 'moeda(s) de R$ 0.50')
+    print(n025, 'moeda(s) de R$ 0.25')
+    print(n010, 'moeda(s) de R$ 0.10')
+    print(n005, 'moeda(s) de R$ 0.05')
+    print(n001, 'moeda(s) de R$ 0.01')
